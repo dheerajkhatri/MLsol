b4735ca4302b2a53e303678dc011733de93c6444
d-khatri <er.dheerajkhatri@gmail.com>
2014-10-13 20:08:08 +0530

    template

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int,int>   PI;
+typedef vector<int> VI;
+typedef long long LL;
+ 
+#define FOR(i,a,b) for(register int i=a;i<(b);i++)
+#define FORE(i,a,b) FOR(i,a,(b)+1)
+#define FORD(i,a,b) for(int i=a;i>=(b);i--)
+#define REP(i,n) FOR(i,0,n)
+#define PB push_back
+#define mod 1000000007
+#define MP make_pair
+
+inline int toInt(string s) {int i;stringstream (s)>>i;return i;} 
+inline string toString(int i) {string s;stringstream ss;ss<<i;ss>>s;return s;}
+
+int main(){
+	
+
+	return 0;
+}
+
